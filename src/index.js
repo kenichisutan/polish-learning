b855752309a8d2129ccceeb22cddd0a24d8761da
kenichisutan <kenichi.jks@gmail.com>
2023-10-11 15:54:39 +0900

    Created template files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {BrowserRouter, Routes, Route, Outlet, Navigate} from 'react-router-dom';
 import App from './App';
+import Home from './components/Home';
+import ErrorPage from "./components/ErrorPage";
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+
+const router = (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} errorElement={<ErrorPage />}>
+                <Route index element={<Home />} />
+                {/*<Route path="/login" element={<Login />} />*/}
+                {/* Catch-all route for unmatched routes */}
+                <Route path="*" element={<ErrorPage />} /> {/* Catch-all route for unmatched routes */}
+            </Route>
+        </Routes>
+    </BrowserRouter>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {router}
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
