c38982f83d93c2c5a624991817d043b0bbf0bc3f
kenichisutan <kenichi.jks@gmail.com>
2023-10-12 20:23:29 +0900

    Declension logic placed into declensions class
    Declensions class works as an object, all information stored in the object itself. Function can refer to self.

--- src/classes/Declensions.js
+++ src/classes/Declensions.js
@@ -1,7 +1,8 @@
-class Declensions {
+class declension {
     constructor() {
         this.singular = this.createCaseObject();
         this.plural = this.createCaseObject();
+        this.gender = "";
     }
 
     createCaseObject() {
@@ -15,6 +16,218 @@ class Declensions {
             vocative: ""
         };
     }
+
+    setGender(gender) {
+        this.gender = gender;
+    }
+
+    decline(word) {
+        // check gender of word
+
+        // ---------------MASCULINE----------------
+
+        // ---------------FEMININE----------------
+
+        // ---------------NEUTER----------------
+        if(word[word.length - 1] === 'o' || word[word.length - 1] === 'e' || word[word.length - 1] === 'ę') {
+            this.declineNeuter(word);
+            this.setGender("Neuter")
+        }
+    }
+
+    declineNeuter(word) {
+        // decide which method to use based on the last letter
+        if(word[word.length - 1] === 'o') {
+            this.declineNeuterSingularEndingO(word);
+            this.declineNeuterPluralEndingO(word);
+        }
+        else if(word[word.length - 1] === 'e') {
+            this.declineNeuterSingularEndingE(word);
+            this.declineNeuterPluralEndingE(word);
+        }
+        else if(word[word.length - 1] === 'ę') {
+            this.declineNeuterSingularEndingEzOgonkiem(word);
+            this.declineNeuterPluralEndingEzOgonkiem(word);
+        }
+    }
+
+    declineNeuterSingularEndingO(word) {
+        // ---------------SINGULAR O----------------
+        // ---------------NOMINATIVE, ACC., VOC.----------------
+        this.singular.nominative =
+            this.singular.accusative =
+                this.singular.vocative = word;
+        // ---------------GENITIVE----------------
+        this.singular.genitive = word.substring(0, word.length - 1) + 'a';
+        // ---------------DATIVE----------------
+        this.singular.dative = word.substring(0, word.length - 1) + 'u';
+        // ---------------INSTRUMENTAL----------------
+        this.singular.instrumental = word.substring(0, word.length - 1) + 'em';
+        // --------Jabłko -> Jabłkiem--------
+        // if ends with ko, switch to iem
+        if(word[word.length - 2] === 'k') {
+            this.singular.instrumental = word.substring(0, word.length - 1) + 'iem';
+        }
+        // ---------------LOCATIVE----------------
+        this.singular.locative = word.substring(0, word.length - 1) + 'ie';
+        // --------Koło -> Kole--------
+        // if ends with ł, switch to le
+        if(word[word.length - 2] === 'ł') {
+            this.singular.locative = word.substring(0, word.length - 2) + 'le';
+        }
+        // --------Jabłko -> Jabłku--------
+        // if ends with ko, make same as dative
+        if(word[word.length - 2] === 'k') {
+            this.singular.locative = this.singular.dative;
+        }
+    }
+
+    declineNeuterSingularEndingE(word) {
+        // ---------------SINGULAR E----------------
+        // ---------------NOMINATIVE, ACC., VOC.----------------
+        this.singular.nominative =
+            this.singular.accusative =
+                this.singular.vocative = word;
+        // ---------------GENITIVE----------------
+        this.singular.genitive = word.substring(0, word.length - 1) + 'a';
+        // ---------------DATIVE----------------
+        this.singular.dative = word.substring(0, word.length - 1) + 'u';
+        // ---------------INSTRUMENTAL----------------
+        this.singular.instrumental = word.substring(0, word.length - 1) + 'em';
+        // ---------------LOCATIVE----------------
+        this.singular.locative = word.substring(0, word.length - 1) + 'u';
+    }
+
+    declineNeuterSingularEndingEzOgonkiem(word) {
+        // ---------------SINGULAR E Z OGONKIEM----------------
+        // -------- Imię -> Imienia --------
+        // if 2nd last letter is a vowel or an i
+        if(word[word.length - 2] === 'i') {
+            // ---------------NOMINATIVE, ACC., VOC.----------------
+            this.singular.nominative =
+                this.singular.accusative =
+                    this.singular.vocative = word;
+            // ---------------GENITIVE----------------
+            this.singular.genitive = word.substring(0, word.length - 1) + 'enia';
+            // ---------------DATIVE----------------
+            this.singular.dative = word.substring(0, word.length - 1) + 'eniu';
+            // ---------------INSTRUMENTAL----------------
+            this.singular.instrumental = word.substring(0, word.length - 1) + 'eniem';
+            // ---------------LOCATIVE----------------
+            this.singular.locative = word.substring(0, word.length - 1) + 'eniu';
+        }
+        // -------- Zwierzę -> Zwierzęciem --------
+        // if does not have an i in the 2nd last position
+        else {
+            // ---------------NOMINATIVE, ACC., VOC.----------------
+            this.singular.nominative =
+                this.singular.accusative =
+                    this.singular.vocative = word;
+            // ---------------GENITIVE----------------
+            this.singular.genitive = word + 'cia';
+            // ---------------DATIVE----------------
+            this.singular.dative = word + 'ciu';
+            // ---------------INSTRUMENTAL----------------
+            this.singular.instrumental = word + 'ciem';
+            // ---------------LOCATIVE----------------
+            this.singular.locative = word + 'ciu';
+        }
+    }
+
+    declineNeuterPluralEndingO(word) {
+        // ---------------PLURAL O----------------
+        // ---------------NOMINATIVE, ACC., VOC.----------------
+        this.plural.nominative =
+            this.plural.accusative =
+                this.plural.vocative = word.substring(0, word.length - 1) + 'a';
+        // ---------------GENITIVE----------------
+        this.plural.genitive = word.substring(0, word.length - 1);
+        // --------Kóło -> Kól--------
+        // if contains an o vowel outside the first, locate the second last o
+        let lastO = word.indexOf('o', 1);
+        if(lastO !== -1 && lastO !== word.length - 1) {
+            this.plural.genitive = word.substring(0, lastO) + 'ó' + word.substring(lastO + 1, word.length - 1);
+        }
+        // --------Jabłko -> Jabłek--------
+        // if ends with ko, switch to ek
+        if(word[word.length - 2] === 'k') {
+            this.plural.genitive = word.substring(0, word.length - 2) + 'ek';
+        }
+        // ---------------DATIVE----------------
+        this.plural.dative = word.substring(0, word.length - 1) + 'om';
+        // ---------------INSTRUMENTAL----------------
+        this.plural.instrumental = word.substring(0, word.length - 1) + 'ami';
+        // ---------------LOCATIVE----------------
+        this.plural.locative = word.substring(0, word.length - 1) + 'ach';
+    }
+
+    declineNeuterPluralEndingE(word) {
+        // ---------------PLURAL E----------------
+        // ---------------NOMINATIVE, ACC., VOC.----------------
+        this.plural.nominative =
+            this.plural.accusative =
+                this.plural.vocative = word.substring(0, word.length - 1) + 'a';
+        // ---------------GENITIVE----------------
+        this.plural.genitive = word.substring(0, word.length - 1);
+        // --------Pole -> Pól--------
+        // if contains an o vowel, locate the last o
+        let lastO = word.indexOf('o');
+        if(lastO !== -1) {
+            this.plural.genitive = word.substring(0, lastO) + 'ó' + word.substring(lastO + 1, word.length - 1);
+        }
+        // --------Jedzenie -> Jedzeń--------
+        // if ends with nie, switch to ń
+        if(word[word.length - 3] === 'n') {
+            this.plural.genitive = word.substring(0, word.length - 3) + 'ń';
+        }
+        // --------Picie -> Pić--------
+        // if ends with cie, switch to ć
+        else if(word[word.length - 3] === 'c') {
+            this.plural.genitive = word.substring(0, word.length - 3) + 'ć';
+        }
+        // ---------------DATIVE----------------
+        this.plural.dative = word.substring(0, word.length - 1) + 'om';
+        // ---------------INSTRUMENTAL----------------
+        this.plural.instrumental = word.substring(0, word.length - 1) + 'ami';
+        // ---------------LOCATIVE----------------
+        this.plural.locative = word.substring(0, word.length - 1) + 'ach';
+    }
+
+    declineNeuterPluralEndingEzOgonkiem(word) {
+        // ---------------PLURAL E Z OGONKIEM----------------
+        // -------- Imię -> Imion --------
+        // if 2nd last letter is a vowel or an i
+        if(word[word.length - 2] === 'i') {
+            // ---------------NOMINATIVE, ACC., VOC.----------------
+            this.plural.nominative =
+                this.plural.accusative =
+                    this.plural.vocative = word.substring(0, word.length - 1) + 'ona';
+            // ---------------GENITIVE----------------
+            this.plural.genitive = word.substring(0, word.length - 1) + 'on';
+            // ---------------DATIVE----------------
+            this.plural.dative = word.substring(0, word.length - 1) + 'onom';
+            // ---------------INSTRUMENTAL----------------
+            this.plural.instrumental = word.substring(0, word.length - 1) + 'onami';
+            // ---------------LOCATIVE----------------
+            this.plural.locative = word.substring(0, word.length - 1) + 'onach';
+        }
+        // -------- Zwierzę -> Zwierzęta --------
+        // if does not have an i in the 2nd last position
+        else {
+            // ---------------NOMINATIVE, ACC., VOC.----------------
+            this.plural.nominative =
+                this.plural.accusative =
+                    this.plural.vocative = word + 'ta';
+            // ---------------GENITIVE----------------
+            this.plural.genitive = word.substring(0, word.length - 1) + 'ąt';
+            // ---------------DATIVE----------------
+            this.plural.dative = word + 'tom';
+            // ---------------INSTRUMENTAL----------------
+            this.plural.instrumental = word + 'tami';
+            // ---------------LOCATIVE----------------
+            this.plural.locative = word + 'tach';
+        }
+    }
 }
 
-export default Declensions;
+export default declension;
