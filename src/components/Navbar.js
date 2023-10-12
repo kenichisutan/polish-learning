14b2fd7ad97e9a4ef2dae511b30780d516764786
kenichisutan <kenichi.jks@gmail.com>
2023-10-12 19:12:58 +0900

    Modified navbar to use buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,18 +14,24 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            {/*<button type="button" className={"btn btn-danger"}>*/}
+                            <button type="button" className={"btn btn-danger btn-sm"}>
                                 <Link to={"/home"} className={"nav-link"}>Home</Link>
-                            {/*</button>*/}
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <Link to={"/dictionary"} className={"nav-link"}>Dictionary</Link>
+                            <button type="button" className={"btn btn-danger btn-sm"}>
+                                <Link to={"/dictionary"} className={"nav-link"}>Dictionary</Link>
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <Link to={"/conjugation"} className={"nav-link"}>Conjugation</Link>
+                            <button type="button" className={"btn btn-danger btn-sm"}>
+                                <Link to={"/conjugation"} className={"nav-link"}>Conjugation</Link>
+                            </button>
                         </li>
                         <li className="nav-item">
-                            <Link to={"/declension"} className={"nav-link"}>Declension</Link>
+                            <button type="button" className={"btn btn-danger btn-sm"}>
+                                <Link to={"/declension"} className={"nav-link"}>Declension</Link>
+                            </button>
                         </li>
                     </ul>
                 </div>
