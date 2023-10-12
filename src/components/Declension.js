fc0751a6880482bb5e8736a8fb0b1051c71c0b87
kenichisutan <kenichi.jks@gmail.com>
2023-10-12 14:03:53 +0900

    Created declension form

--- src/components/Declension.js
+++ src/components/Declension.js
@@ -2,6 +2,17 @@ const Declension = () => {
     return (
         <div>
             <h1>Declension</h1>
+            <br />
+            <h3>Enter a word to decline</h3>
+            <br />
+            <form>
+                <div className="form-group">
+                    <label htmlFor="inputDeclension1">Word</label>
+                    <input type="word" className="form-control" id="inputDeclension1" aria-describedby="declensionHelp" placeholder="Enter a word" />
+                    <small id="declensionHelp" className="form-text text-muted">Test</small>
+                </div>
+                <button type="submit" className="btn btn-primary">Decline</button>
+            </form>
         </div>
     )
 }
