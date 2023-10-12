fc3e3345b6362298958a69bda6f3046966342ee4
kenichisutan <kenichi.jks@gmail.com>
2023-10-12 15:04:51 +0900

    Created functionality for neuter nouns

--- src/components/Declension.js
+++ src/components/Declension.js
@@ -1,34 +1,71 @@
 import {useState} from "react";
 import DeclensionTable from "./DeclensionTable";
+import Declensions from "../classes/Declensions";
 
 const Declension = () => {
     const [selectedDeclension, setSelectedDeclension] = useState(null);
 
     function handleSubmit(event) {
         event.preventDefault();
+        const word = document.getElementById('inputDeclension1').value;
+        let declensions = new Declensions();
 
-        let declension = {
-            singular: {
-                nominative: "Piwo",
-                genitive: "Piwa",
-                dative: "Piwu",
-                accusative: "Piwo",
-                instrumental: "Piwo",
-                locative: "Piwie",
-                vocative: "Piwem"
-            },
-            plural: {
-                nominative: "Piwa",
-                genitive: "Piw",
-                dative: "Piwom",
-                accusative: "Piwa",
-                instrumental: "Piwami",
-                locative: "Piwach",
-                vocative: "Piwami"
-            }
+        // determine if word is masculine, feminine, or neuter
+        const lastLetter = word[word.length - 1];
+        if(lastLetter === 'o' || lastLetter === 'e' || lastLetter === 'ę') {
+            declineNeuter(word, declensions);
         }
 
-        setSelectedDeclension(declension);
+        setSelectedDeclension(declensions);
+    }
+
+    function declineNeuter(word, declensions) {
+        // check if last letter is o
+        if(word[word.length - 1] === 'o') {
+            declensions.singular.nominative =
+                declensions.singular.accusative =
+                declensions.singular.vocative = word;
+            declensions.singular.genitive = word.substring(0, word.length - 1) + 'a';
+            declensions.singular.dative = word.substring(0, word.length - 1) + 'u';
+            declensions.singular.instrumental = word.substring(0, word.length - 1) + 'em';
+            declensions.singular.locative = word.substring(0, word.length - 1) + 'ie';
+
+            declensions.plural.nominative =
+                declensions.plural.accusative =
+                declensions.plural.vocative = word.substring(0, word.length - 1) + 'a';
+            declensions.plural.genitive = word.substring(0, word.length - 1);
+            declensions.plural.dative = word.substring(0, word.length - 1) + 'om';
+            declensions.plural.instrumental = word.substring(0, word.length - 1) + 'ami';
+            declensions.plural.locative = word.substring(0, word.length - 1) + 'ach';
+        }
+
+        // check if last letter is e
+        if(word[word.length - 1] === 'e' || word[word.length - 1] === 'ę') {
+            declensions.singular.nominative =
+                declensions.singular.accusative =
+                declensions.singular.vocative = word;
+            declensions.singular.genitive = word.substring(0, word.length - 1) + 'a';
+            declensions.singular.dative = word.substring(0, word.length - 1) + 'u';
+            declensions.singular.instrumental = word.substring(0, word.length - 1) + 'em';
+            declensions.singular.locative = word.substring(0, word.length - 1) + 'u';
+
+            declensions.plural.nominative =
+                declensions.plural.accusative =
+                declensions.plural.vocative = word.substring(0, word.length - 1) + 'a';
+
+            // if ends with nie, switch to ń
+            if(word[word.length - 3] === 'n') {
+                declensions.plural.genitive = word.substring(0, word.length - 3) + 'ń';
+            }
+            // if ends with cie, switch to ć
+            else if(word[word.length - 3] === 'c') {
+                declensions.plural.genitive = word.substring(0, word.length - 3) + 'ć';
+            }
+
+            declensions.plural.dative = word.substring(0, word.length - 1) + 'om';
+            declensions.plural.instrumental = word.substring(0, word.length - 1) + 'ami';
+            declensions.plural.locative = word.substring(0, word.length - 1) + 'ach';
+        }
     }
 
     return (
