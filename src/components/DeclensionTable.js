21c0b79e76042360ea0c594e0b11bab0b7ee039a
kenichisutan <kenichi.jks@gmail.com>
2023-10-12 15:22:02 +0900

    Added declension styling

--- src/components/DeclensionTable.js
+++ src/components/DeclensionTable.js
@@ -1,3 +1,5 @@
+import "../styles/Declension.css"
+
 const DeclensionTable = ({ declension }) => {
     return (
         <div className="table-responsive">
@@ -12,49 +14,49 @@ const DeclensionTable = ({ declension }) => {
                 <tbody>
                     <tr>
                         <th scope="col">Nominative
-                            (kto? co?)
+                            <span className="table-detail">(kto? co?)</span>
                         </th>
                         <td>{declension.singular.nominative}</td>
                         <td>{declension.plural.nominative}</td>
                     </tr>
                     <tr>
                         <th scope="col">Genitive
-                            (kogo? czego?)
+                            <span className="table-detail">(kogo? czego?)</span>
                         </th>
                         <td>{declension.singular.genitive}</td>
                         <td>{declension.plural.genitive}</td>
                     </tr>
                     <tr>
                         <th scope="col">Dative
-                            (komu? czemu?)
+                            <span className="table-detail">(komu? czemu?)</span>
                         </th>
                         <td>{declension.singular.dative}</td>
                         <td>{declension.plural.dative}</td>
                     </tr>
                     <tr>
                         <th scope="col">Accusative
-                            (kogo? co?)
+                            <span className="table-detail">(kogo? co?)</span>
                         </th>
                         <td>{declension.singular.accusative}</td>
                         <td>{declension.plural.accusative}</td>
                     </tr>
                     <tr>
                         <th scope="col">Instrumental
-                            ((z) kim? (z) czym?)
+                            <span className="table-detail">((z) kim? (z) czym?)</span>
                         </th>
                         <td>{declension.singular.instrumental}</td>
                         <td>{declension.plural.instrumental}</td>
                     </tr>
                     <tr>
                         <th scope="col">Locative
-                            (o kim? o czym?)
+                            <span className="table-detail">(o kim? o czym?)</span>
                         </th>
                         <td>{declension.singular.locative}</td>
                         <td>{declension.plural.locative}</td>
                     </tr>
                     <tr>
                         <th scope="col">Vocative
-                            (o!)
+                            <span className="table-detail">(o!)</span>
                         </th>
                         <td>{declension.singular.vocative}</td>
                         <td>{declension.plural.vocative}</td>
