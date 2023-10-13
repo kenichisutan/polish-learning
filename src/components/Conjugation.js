f8471465095a9b557079036fafa7c8a9fba49228
kenichisutan <kenichi.jks@gmail.com>
2023-10-13 12:45:10 +0900

    Added the entry form for conjugation

--- src/components/Conjugation.js
+++ src/components/Conjugation.js
@@ -1,7 +1,20 @@
 const Conjugation = () => {
+    const handleSubmit = (event) => {
+
+    }
+
     return (
         <div className="page-content">
-            <h1>Declension</h1>
+            <h1>Conjugation</h1>
+            <br />
+            <form>
+                <div className="form-group">
+                    <label htmlFor="inputConjugation1">Enter a word to conjugate</label>
+                    <input type="word" className="form-control" id="inputConjugation1" aria-describedby="conjugationHelp" placeholder="mówić" />
+                    <small id="conjugationHelp" className="form-text text-muted">The entered word must be a verb in the infinitive form.</small>
+                </div>
+                <button type="submit" className="btn btn-outline-danger" onClick={handleSubmit}>Conjugate</button>
+            </form>
             <br />
         </div>
     )
