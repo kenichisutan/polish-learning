a80e5cb66a452a8ac737882d04636bf49194ff63
kenichisutan <kenichi.jks@gmail.com>
2023-10-13 14:40:56 +0900

    Migrated strings to a json file in resources

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import {Link} from "react-router-dom";
 import "../styles/Navbar.css";
+import str from "../res/values/strings/strings.json";
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg fixed-top" data-bs-theme={"dark"}>
             <div className="container-fluid">
-                <Link to={"/"} className={"navbar-brand"}>Polish Learning</Link>
+                <Link to={"/"} className={"navbar-brand"}>{str.page_title}</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
                         data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false"
                         aria-label="Toggle navigation">
@@ -15,22 +16,22 @@ const Navbar = () => {
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <button type="button" className={"btn btn-danger btn-sm"}>
-                                <Link to={"/home"} className={"nav-link"}>Home</Link>
+                                <Link to={"/home"} className={"nav-link"}>{str.navbar_home}</Link>
                             </button>
                         </li>
                         <li className="nav-item">
                             <button type="button" className={"btn btn-danger btn-sm"}>
-                                <Link to={"/dictionary"} className={"nav-link"}>Dictionary</Link>
+                                <Link to={"/dictionary"} className={"nav-link"}>{str.navbar_dictionary}</Link>
                             </button>
                         </li>
                         <li className="nav-item">
                             <button type="button" className={"btn btn-danger btn-sm"}>
-                                <Link to={"/conjugation"} className={"nav-link"}>Conjugation</Link>
+                                <Link to={"/conjugation"} className={"nav-link"}>{str.navbar_conjugation}</Link>
                             </button>
                         </li>
                         <li className="nav-item">
                             <button type="button" className={"btn btn-danger btn-sm"}>
-                                <Link to={"/declension"} className={"nav-link"}>Declension</Link>
+                                <Link to={"/declension"} className={"nav-link"}>{str.navbar_declension}</Link>
                             </button>
                         </li>
                     </ul>
