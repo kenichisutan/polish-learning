c9c3123174c1c2ad3d4c17ea2ade2e1892e6fcc3
kenichisutan <kenichi.jks@gmail.com>
2023-10-13 10:45:18 +0900

    Added placeholder dictionary search feature

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -1,9 +1,27 @@
 import "../styles/Dictionary.css";
 
 const Dictionary = () => {
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const entry = document.getElementById('inputDictionary1').value;
+        // uncapitalize the first letter
+        const lowerCaseEntry = entry.charAt(0).toLowerCase() + entry.slice(1);
+        console.log(lowerCaseEntry);
+        // dictionary search logic
+    }
+
     return (
         <div className="page-content">
             <h1>Dictionary</h1>
+            <br />
+            <form>
+                <div className="form-group">
+                    <label htmlFor="inputDictionary1">Enter a word to search</label>
+                    <input type="word" className="form-control" id="inputDictionary1" aria-describedby="dictionaryHelp" placeholder="śniadanie" />
+                    <small id="dictionaryHelp" className="form-text text-muted">Enter a Polish word above.</small>
+                </div>
+                <button type="submit" className="btn btn-outline-danger" onClick={handleSubmit}>Search</button>
+            </form>
         </div>
     )
 }
